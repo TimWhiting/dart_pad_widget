091016e1c47b9fee6eab50d4a4a74d46c61c5793
Tim Whiting <tim@whitings.org>
2020-04-21 12:41:14 -0600

    update license

--- lib/dart_pad_widget.dart
+++ lib/dart_pad_widget.dart
@@ -1,3 +1,5 @@
+// Copyright (c) 2020, Tim Whiting. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 library dart_pad;
 
 import 'dart:html' as html;
